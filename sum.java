39c06f3ccb2520da17f9934cd85f68f675d7e338
devopsnhck22 <126224087+devopsnhck22@users.noreply.github.com>
2023-02-28 13:56:20 +0530

    first commit

--- sum.java
+++ sum.java
@@ -1,5 +1,5 @@
 //this is for jenkins
-import java.util.Scanner;
+import java.util.*;
 public class sum
 { 
    public static void main(String args[]) 
@@ -7,8 +7,8 @@ public class sum
       int x, y, z; 
       System.out.println("Enter two integers to calculate their sum "); 
       Scanner in = new Scanner(System.in); 
-      x = in.nextInt(); 
-      y = in.nextInt(); 
+      x = integer.parseInt(args[0]); 
+      y = integer.parseInt(args[1]);  
       z = x + y; 
       System.out.println("Sum of entered integers = "+z); 
    } 
